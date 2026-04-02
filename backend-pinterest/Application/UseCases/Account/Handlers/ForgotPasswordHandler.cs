98b01043ce99d7f8c3334576c157457b115921ff
Taras <rringoo043@gmail.com>
2026-04-03 02:43:31 +0300

    Added ForgotPasswordHandler

---
+++ backend-pinterest/Application/UseCases/Account/Handlers/ForgotPasswordHandler.cs
@@ -0,0 +1,12 @@
+﻿using Application.UseCases.Account.Commands;
+using MediatR;
+
+namespace Application.UseCases.Account.Handlers;
+
+public class ForgotPasswordHandler : IRequestHandler<ForgotPasswordCommand, bool>
+{
+    public Task<bool> Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
