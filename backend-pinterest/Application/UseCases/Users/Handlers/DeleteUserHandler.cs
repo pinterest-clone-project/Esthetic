ad57298d68a98dafae2489634831d33e84c2d9d2
Taras <rringoo043@gmail.com>
2026-04-04 21:58:02 +0300

    Refactor delete

--- backend-pinterest/Application/UseCases/Users/Handlers/DeleteUserHandler.cs
+++ backend-pinterest/Application/UseCases/Users/Handlers/DeleteUserHandler.cs
@@ -4,14 +4,12 @@ using Application.UseCases.Users.Commands;
 
 namespace Application.UseCases.Users.Handlers;
 
-public class DeleteUserHandler : IRequestHandler<DeleteUserCommand, Unit>
+public class DeleteUserHandler(
+    IUserRepository userRepository) : IRequestHandler<DeleteUserCommand, Unit>
 {
-    private readonly IUserRepository _repo;
-    public DeleteUserHandler(IUserRepository repo) => _repo = repo;
-
     public async Task<Unit> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
     {
-        await _repo.DeleteAsync(request.Id);
+        await userRepository.DeleteAsync(request.Id);
         return Unit.Value;
     }
 }
