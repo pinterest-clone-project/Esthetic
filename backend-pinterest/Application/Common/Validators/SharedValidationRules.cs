b35c1c3831b0e154845810d688d6ca4f2c5c8032
Taras <rringoo043@gmail.com>
2026-04-04 18:08:49 +0300

    Changed edit validator

--- backend-pinterest/Application/Common/Validators/SharedValidationRules.cs
+++ backend-pinterest/Application/Common/Validators/SharedValidationRules.cs
@@ -4,8 +4,7 @@ namespace Application.Common.Validators;
 
 public static class SharedValidationRules
 {
-    public static IRuleBuilderOptions<T, string> EmailRules<T>(
-        this IRuleBuilder<T, string> rule) =>
+    public static IRuleBuilderOptions<T, string?> EmailRules<T>(this IRuleBuilder<T, string?> rule) =>
         rule
             .NotEmpty()
             .WithMessage("Email є обов'язковим")
@@ -14,8 +13,7 @@ public static class SharedValidationRules
             .MaximumLength(255)
             .WithMessage("Email не може бути довшим за 255 символів");
 
-    public static IRuleBuilderOptions<T, string> PasswordRules<T>(
-        this IRuleBuilder<T, string> rule) =>
+    public static IRuleBuilderOptions<T, string?> PasswordRules<T>(this IRuleBuilder<T, string?> rule) =>
         rule
             .NotEmpty()
             .WithMessage("Пароль є обов'язковим")
@@ -24,8 +22,7 @@ public static class SharedValidationRules
             .MaximumLength(100)
             .WithMessage("Пароль не може бути довшим за 100 символів");
 
-    public static IRuleBuilderOptions<T, string?> PhoneRules<T>(
-        this IRuleBuilder<T, string?> rule) =>
+    public static IRuleBuilderOptions<T, string?> PhoneRules<T>(this IRuleBuilder<T, string?> rule) =>
         rule
             .Matches(@"^\+?[0-9\s\-\(\)]{7,20}$")
             .WithMessage("Невірний формат номера телефону")
@@ -33,16 +30,15 @@ public static class SharedValidationRules
             .WithMessage("Номер телефону не може бути довшим за 20 символів")
             .When(x => x is string s && !string.IsNullOrEmpty(s));
 
-    public static IRuleBuilderOptions<T, string> NameRules<T>(
-        this IRuleBuilder<T, string> rule, string fieldName) =>
+    public static IRuleBuilderOptions<T, string?> NameRules<T>(this IRuleBuilder<T, string?> rule, string fieldName) =>
         rule
             .NotEmpty()
             .WithMessage($"{fieldName} є обов'язковим")
             .MaximumLength(50)
-            .WithMessage($"{fieldName} не може бути довшим за 50 символів");
+            .WithMessage($"{fieldName} не може бути довшим за 50 символів")
+            .When(x => x is string s && !string.IsNullOrEmpty(s));
 
-    public static IRuleBuilderOptions<T, string?> BioRules<T>(
-        this IRuleBuilder<T, string?> rule) =>
+    public static IRuleBuilderOptions<T, string?> BioRules<T>(this IRuleBuilder<T, string?> rule) =>
         rule
             .MaximumLength(500)
             .WithMessage("Біографія не може бути довшою за 500 символів");
