a12c19a3bc3acdbd5c0af1ec5d1887578e6b9acd
Taras <rringoo043@gmail.com>
2026-04-07 12:28:54 +0300

    Added TransactionBehavior

---
+++ backend-pinterest/Application/Behaviors/TransactionBehavior.cs
@@ -0,0 +1,34 @@
+﻿using Application.Interfaces;
+using Application.Interfaces.Transaction;
+using MediatR;
+
+namespace Application.Behaviors;
+
+public class TransactionBehavior<TRequest, TResponse>(IAppDbContext db)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ITransactionalCommand
+{
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (db.CurrentTransaction != null)
+            return await next();
+
+        await using var transaction = await db
+            .BeginTransactionAsync(cancellationToken);
+
+        try
+        {
+            var response = await next();
+            await transaction.CommitAsync(cancellationToken);
+            return response;
+        }
+        catch
+        {
+            await transaction.RollbackAsync(cancellationToken);
+            throw;
+        }
+    }
+}
