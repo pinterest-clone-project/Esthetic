86d4e76e951584b04d956f049e6824d61161bd8b
P-Nastia <gogego1999@sectorid.com>
2026-04-07 20:22:24 +0300

    adapt code

--- backend-pinterest/Application/UseCases/Tags/Queries/GetAllTagsQuery.cs
+++ backend-pinterest/Application/UseCases/Tags/Queries/GetAllTagsQuery.cs
@@ -1,8 +1,5 @@
 ﻿
 using Application.Models.DTO.Tag;
-using Application.Models.DTO.User;
-using Domain.Constants;
-using Domain.Interfaces.Caching;
 using MediatR;
 
 namespace Application.UseCases.Tags.Queries;
