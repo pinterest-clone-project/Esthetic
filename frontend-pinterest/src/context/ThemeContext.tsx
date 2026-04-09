ea586d76e02915746c700d2d1e7c7e9a85714188
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-04-09 19:44:07 +0300

    Add ThemeContext

---
+++ frontend-pinterest/src/context/ThemeContext.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React, {useContext} from "react";
+import { createContext, useState, useEffect } from "react";
+
+type Theme = "light" | "dark";
+
+type ThemeContextType = {
+    theme: Theme;
+    toggleTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
+                                                                           children,
+                                                                       }) => {
+    const [theme, setTheme] = useState<Theme>("light");
+    const [isInitialized, setIsInitialized] = useState(false);
+
+    useEffect(() => {
+        // This code will only run on the client side
+        const savedTheme = localStorage.getItem("theme") as Theme | null;
+        let initialTheme: Theme;
+
+        if (savedTheme) {
+            initialTheme = savedTheme;
+        } else {
+            const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+            initialTheme = prefersDark ? "dark" : "light";
+        } // Default to light theme
+
+        setTheme(initialTheme);
+        setIsInitialized(true);
+    }, []);
+
+    useEffect(() => {
+        if (isInitialized) {
+            localStorage.setItem("theme", theme);
+            if (theme === "dark") {
+                document.documentElement.classList.add("dark");
+            } else {
+                document.documentElement.classList.remove("dark");
+            }
+        }
+    }, [theme, isInitialized]);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+    if (context === undefined) {
+        throw new Error("useTheme must be used within a ThemeProvider");
+    }
+    return context;
+};
