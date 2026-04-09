5461d0bd53ef7823c859503c7ee5e72b171b1832
Taras <rringoo043@gmail.com>
2026-04-09 15:27:42 +0300

    Added UnfollowAsync to IFollowRepository

--- backend-pinterest/Domain/Interfaces/IFollowRepository.cs
+++ backend-pinterest/Domain/Interfaces/IFollowRepository.cs
@@ -3,4 +3,5 @@
 public interface IFollowRepository
 {
     Task FollowAsync(Guid followerId, Guid followeeId, CancellationToken ct = default);
+    Task UnfollowAsync(Guid followerId, Guid followeeId, CancellationToken ct = default);
 }
