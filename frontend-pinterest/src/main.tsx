06177cb10cf13dd9d0f92f696944a8a44624e94c
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-04-09 17:59:01 +0300

    Start implement google auth and start working on theme changing

--- frontend-pinterest/src/main.tsx
+++ frontend-pinterest/src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx'
 import {Provider} from "react-redux";
 import {store} from "./store";
 import {BrowserRouter} from "react-router";
+import {GoogleOAuthProvider} from "@react-oauth/google";
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
         <BrowserRouter>
-        <App/>
+            <GoogleOAuthProvider clientId="911542527173-rsr0s8elapousvqvskvqnmns61j9i3ev.apps.googleusercontent.com">
+                <App/>
+            </GoogleOAuthProvider>
         </BrowserRouter>
     </Provider>
 )
