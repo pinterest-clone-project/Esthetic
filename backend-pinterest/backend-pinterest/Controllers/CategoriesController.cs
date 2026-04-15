e9202040f6279de67ecd623029af5385c05e072e
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-15 16:19:30 +0300

    Added GetAll endpoint

--- backend-pinterest/backend-pinterest/Controllers/CategoriesController.cs
+++ backend-pinterest/backend-pinterest/Controllers/CategoriesController.cs
@@ -1,9 +1,17 @@
+using Application.UseCases.Categories.Queries;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend_pinterest.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class CategoriesController : ControllerBase
+public class CategoriesController(IMediator mediator) : ControllerBase
 {
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var categories = await mediator.Send(new GetAllCategoriesQuery());
+        return Ok(categories);
+    }
 }
