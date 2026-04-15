2d13e7a0887f88a233d328593e4682f846235c90
Taras <rringoo043@gmail.com>
2026-04-15 15:42:17 +0300

    Implement mapper

--- backend-pinterest/Application/Mappers/CategoryMapper.cs
+++ backend-pinterest/Application/Mappers/CategoryMapper.cs
@@ -1,7 +1,13 @@
+using Application.Models.DTO.Category;
 using AutoMapper;
+using Domain.Entities.Category;
 
 namespace Application.Mappers;
 
 public class CategoryMapper : Profile
 {
+    public CategoryMapper()
+    {
+        CreateMap<CategoryEntity, CategoryDTO>();
+    }
 }
