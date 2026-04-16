3ab62637abd76bd74c55893bb229cbb6aa102a77
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-16 15:49:28 +0300

    Added new rules for GetCategoryByIdValidator

--- backend-pinterest/Application/UseCases/Categories/Validators/GetCategoryByIdValidator.cs
+++ backend-pinterest/Application/UseCases/Categories/Validators/GetCategoryByIdValidator.cs
@@ -1,8 +1,13 @@
-﻿using Application.UseCases.Categories.Queries;
+﻿using Application.Common.Validators;
+using Application.UseCases.Categories.Queries;
 using FluentValidation;
 
 namespace Application.UseCases.Categories.Validators;
 
 public class GetCategoryByIdValidator : AbstractValidator<GetCategoryByIdQuery>
 {
+    public GetCategoryByIdValidator()
+    {
+        RuleFor(x => x.Id).IdRules();
+    }
 }
