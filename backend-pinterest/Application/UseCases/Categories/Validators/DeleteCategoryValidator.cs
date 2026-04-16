77de3bc0ca87181e4ecc12be4650251085297d80
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-16 15:48:44 +0300

    Added rules for DeleteCategoryValidator

--- backend-pinterest/Application/UseCases/Categories/Validators/DeleteCategoryValidator.cs
+++ backend-pinterest/Application/UseCases/Categories/Validators/DeleteCategoryValidator.cs
@@ -1,8 +1,13 @@
-﻿using Application.UseCases.Categories.Commands;
+﻿using Application.Common.Validators;
+using Application.UseCases.Categories.Commands;
 using FluentValidation;
 
 namespace Application.UseCases.Categories.Validators;
 
 public class DeleteCategoryValidator : AbstractValidator<DeleteCategoryCommand>
 {
+    public DeleteCategoryValidator()
+    {
+        RuleFor(x => x.Id).IdRules();
+    }
 }
