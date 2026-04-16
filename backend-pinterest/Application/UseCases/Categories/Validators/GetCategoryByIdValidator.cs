85ccc881655db008eb57fe80230267a0870d9add
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-16 15:14:37 +0300

    Added GetCategoryByIdValidator

---
+++ backend-pinterest/Application/UseCases/Categories/Validators/GetCategoryByIdValidator.cs
@@ -0,0 +1,8 @@
+﻿using Application.UseCases.Categories.Queries;
+using FluentValidation;
+
+namespace Application.UseCases.Categories.Validators;
+
+public class GetCategoryByIdValidator : AbstractValidator<GetCategoryByIdQuery>
+{
+}
