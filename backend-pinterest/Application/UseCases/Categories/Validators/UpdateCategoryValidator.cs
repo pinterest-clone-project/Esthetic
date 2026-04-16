736ef04e38de10a6db47fd280d7d983348d66ab1
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-16 15:15:18 +0300

    Added UpdateCategoryValidator

---
+++ backend-pinterest/Application/UseCases/Categories/Validators/UpdateCategoryValidator.cs
@@ -0,0 +1,8 @@
+﻿using Application.UseCases.Categories.Commands;
+using FluentValidation;
+
+namespace Application.UseCases.Categories.Validators;
+
+public class UpdateCategoryValidator : AbstractValidator<UpdateCategoryCommand>
+{
+}
