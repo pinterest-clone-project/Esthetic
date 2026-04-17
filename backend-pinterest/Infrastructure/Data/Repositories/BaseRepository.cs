370c0c363128c05da6f69b3fbc6317fd30a4894e
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-17 16:49:56 +0300

    Change BaseRepository

--- backend-pinterest/Infrastructure/Data/Repositories/BaseRepository.cs
+++ backend-pinterest/Infrastructure/Data/Repositories/BaseRepository.cs
@@ -1,46 +1,38 @@
-﻿using Domain.Interfaces;
-using Infrastructure.Data;
+﻿using Domain.Entities.Base;
+using Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using Domain.Entities.Base;
 
 namespace Infrastructure.Data.Repositories;
 
-public class BaseRepository<T> : IBaseRepository<T> where T : class, IEntity
+public class BaseRepository<T>(AppDbContext appDbContext) : IBaseRepository<T> where T : class, IEntity
 {
-    protected readonly AppDbContext _db;
-
-    public BaseRepository(AppDbContext db)
-    {
-        _db = db;
-    }
-
     public async Task<T> AddAsync(T entity, CancellationToken ct = default)
     {
-        await _db.Set<T>().AddAsync(entity, ct);
-        await _db.SaveChangesAsync(ct);
+        await appDbContext.Set<T>().AddAsync(entity, ct);
+        await appDbContext.SaveChangesAsync(ct);
         return entity;
     }
 
     public async Task DeleteAsync(Guid id, CancellationToken ct = default)
     {
-        var entity = await _db.Set<T>().FindAsync([id], ct);
+        var entity = await appDbContext.Set<T>().FindAsync([id], ct);
         if (entity == null) return;
 
         entity.IsDeleted = true;
         entity.UpdatedAt = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
-        _db.Set<T>().Update(entity);
+        appDbContext.Set<T>().Update(entity);
 
-        await _db.SaveChangesAsync(ct);
+        await appDbContext.SaveChangesAsync(ct);
     }
 
     public async Task<List<T>> GetAllAsync(CancellationToken ct = default)
     {
-        return await _db.Set<T>().Where(e => !e.IsDeleted).ToListAsync(ct);
+        return await appDbContext.Set<T>().Where(e => !e.IsDeleted).ToListAsync(ct);
     }
 
     public async Task<T?> GetByIdAsync(Guid id, CancellationToken ct = default)
     {
-        var entity = await _db.Set<T>().FindAsync([id], ct);
+        var entity = await appDbContext.Set<T>().FindAsync([id], ct);
         if (entity == null || entity.IsDeleted) return null;
         return entity;
     }
@@ -48,7 +40,7 @@ public class BaseRepository<T> : IBaseRepository<T> where T : class, IEntity
     public async Task UpdateAsync(T entity, CancellationToken ct = default)
     {
         entity.UpdatedAt = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
-        _db.Set<T>().Update(entity);
-        await _db.SaveChangesAsync(ct);
+        appDbContext.Set<T>().Update(entity);
+        await appDbContext.SaveChangesAsync(ct);
     }
 }
