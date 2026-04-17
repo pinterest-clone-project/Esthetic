877f2aeda8b3c61026713bd36bd0b6a001658983
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-17 18:49:55 +0300

    Implement IPagedService in PagedService

--- backend-pinterest/Infrastructure/Services/PagedService.cs
+++ backend-pinterest/Infrastructure/Services/PagedService.cs
@@ -1,9 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Application.Interfaces;
+using Application.Models.DTO;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Services;
 
-internal class PagedService
+public class PagedService : IPagedService
 {
+    public async Task<PagedResult<TDto>> GetPagedAsync<T, TDto>(
+        IQueryable<T> query,
+        Expression<Func<T, TDto>> selector,
+        int page,
+        int pageSize,
+        CancellationToken ct = default)
+    {
+        var total = await query.CountAsync(ct);
+        var items = await query
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .Select(selector)
+            .ToListAsync(ct);
+
+        return new PagedResult<TDto>
+        {
+            Items = items,
+            TotalCount = total,
+            Page = page,
+            PageSize = pageSize
+        };
+    }
 }
