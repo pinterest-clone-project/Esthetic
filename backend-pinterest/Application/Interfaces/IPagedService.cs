1e15aa2e75052e6128b22e2a09f61b88d6bae3c2
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-17 18:47:40 +0300

    Added GetPagedAsync to IPagedService

--- backend-pinterest/Application/Interfaces/IPagedService.cs
+++ backend-pinterest/Application/Interfaces/IPagedService.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using Application.Models.DTO;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Application.Interfaces;
 
-internal class IPagedService
+public interface IPagedService
 {
+    Task<PagedResult<TDto>> GetPagedAsync<T, TDto>(
+        IQueryable<T> query,
+        Expression<Func<T, TDto>> selector,
+        int page,
+        int pageSize,
+        CancellationToken ct = default);
 }
