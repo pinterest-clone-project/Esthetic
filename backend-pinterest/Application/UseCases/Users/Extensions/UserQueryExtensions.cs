a5d28ad77e6ce9f8f8c0db0737178e1b415f2009
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-17 19:12:22 +0300

    Enhance UserQueryExtensions with filtering and sorting

--- backend-pinterest/Application/UseCases/Users/Extensions/UserQueryExtensions.cs
+++ backend-pinterest/Application/UseCases/Users/Extensions/UserQueryExtensions.cs
@@ -1,9 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Application.Common.Sorting;
+using Application.Common.Sorting.User;
+using Application.UseCases.Users.Queries;
+using Domain.Entities.Identity;
 
 namespace Application.UseCases.Users.Extensions;
 
-internal class UserQueryExtensions
+public static class UserQueryExtensions
 {
+    public static IQueryable<UserEntity> ApplyFilters(
+        this IQueryable<UserEntity> query, SearchUsersQuery filter)
+    {
+        if (!string.IsNullOrWhiteSpace(filter.Search))
+        {
+            var search = filter.Search.ToLower();
+            query = query.Where(u =>
+                (u.UserName != null && u.UserName.ToLower().Contains(search)) ||
+                (u.FirstName != null && u.FirstName.ToLower().Contains(search)) ||
+                (u.LastName != null && u.LastName.ToLower().Contains(search)));
+        }
+
+        if (filter.IsPrivate.HasValue)
+            query = query.Where(u => u.IsPrivate == filter.IsPrivate.Value);
+
+        if (filter.IsBlocked.HasValue)
+            query = query.Where(u => u.IsBlocked == filter.IsBlocked.Value);
+
+        return query;
+    }
+
+    public static IQueryable<UserEntity> ApplySorting(
+        this IQueryable<UserEntity> query, SearchUsersQuery filter)
+    {
+        return (filter.SortBy, filter.SortDirection) switch
+        {
+            (UserSortBy.CreatedAt, SortDirection.Asc) => query.OrderBy(u => u.CreatedAt),
+            (UserSortBy.CreatedAt, SortDirection.Desc) => query.OrderByDescending(u => u.CreatedAt),
+            (UserSortBy.UserName, SortDirection.Asc) => query.OrderBy(u => u.UserName),
+            (UserSortBy.UserName, SortDirection.Desc) => query.OrderByDescending(u => u.UserName),
+            _ => query.OrderBy(u => u.CreatedAt)
+        };
+    }
 }
