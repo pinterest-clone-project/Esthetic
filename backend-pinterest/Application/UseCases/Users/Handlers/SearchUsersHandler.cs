d23b64adec0e6c58e6a3d78393363eabf5ab1e3d
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-17 19:23:30 +0300

    Refactor repositories and update user search handler

--- backend-pinterest/Application/UseCases/Users/Handlers/SearchUsersHandler.cs
+++ backend-pinterest/Application/UseCases/Users/Handlers/SearchUsersHandler.cs
@@ -1,9 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Application.Interfaces;
+using Application.Models.DTO;
+using Application.Models.DTO.User;
+using Application.UseCases.Users.Extensions;
+using Application.UseCases.Users.Queries;
+using Domain.Interfaces;
+using MediatR;
 
 namespace Application.UseCases.Users.Handlers;
 
-internal class SearchUsersHandler
+public class SearchUsersHandler(
+    IUserRepository userRepository,
+    IPagedService pagedService) : IRequestHandler<SearchUsersQuery, PagedResult<UserDTO>>
 {
+    public async Task<PagedResult<UserDTO>> Handle(SearchUsersQuery request, CancellationToken cancellationToken)
+    {
+        var q = userRepository.GetQueryable()
+            .ApplyFilters(request)
+            .ApplySorting(request);
+
+        return await pagedService.GetPagedAsync(
+            q,
+            u => new UserDTO
+            {
+                Id = u.Id,
+                UserName = u.UserName,
+                Email = u.Email,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Bio = u.Bio,
+                Image = u.Image,
+                IsPrivate = u.IsPrivate,
+                CreatedAt = u.CreatedAt,
+                UpdatedAt = u.UpdatedAt
+            },
+            request.Page,
+            request.PageSize,
+            cancellationToken
+        );
+    }
 }
--- backend-pinterest/Infrastructure/Data/Repositories/BaseRepository.cs
+++ backend-pinterest/Infrastructure/Data/Repositories/BaseRepository.cs
@@ -4,41 +4,47 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data.Repositories;
 
-public class BaseRepository<T>(AppDbContext appDbContext) : IBaseRepository<T> where T : class, IEntity
+public class BaseRepository<T> : IBaseRepository<T> where T : class, IEntity
 {
+    protected readonly AppDbContext _db;
+
+    public BaseRepository(AppDbContext db)
+    {
+        _db = db;
+    }
     public async Task<T> AddAsync(T entity, CancellationToken ct = default)
     {
-        await appDbContext.Set<T>().AddAsync(entity, ct);
-        await appDbContext.SaveChangesAsync(ct);
+        await _db.Set<T>().AddAsync(entity, ct);
+        await _db.SaveChangesAsync(ct);
         return entity;
     }
 
     public async Task DeleteAsync(Guid id, CancellationToken ct = default)
     {
-        var entity = await appDbContext.Set<T>().FindAsync([id], ct);
+        var entity = await _db.Set<T>().FindAsync([id], ct);
         if (entity == null) return;
 
         entity.IsDeleted = true;
         entity.UpdatedAt = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
-        appDbContext.Set<T>().Update(entity);
+        _db.Set<T>().Update(entity);
 
-        await appDbContext.SaveChangesAsync(ct);
+        await _db.SaveChangesAsync(ct);
     }
 
     public async Task<List<T>> GetAllAsync(CancellationToken ct = default)
     {
-        return await appDbContext.Set<T>().Where(e => !e.IsDeleted).ToListAsync(ct);
+        return await _db.Set<T>().Where(e => !e.IsDeleted).ToListAsync(ct);
     }
 
     public async Task<T?> GetByIdAsync(Guid id, CancellationToken ct = default)
     {
-        var entity = await appDbContext.Set<T>().FindAsync([id], ct);
+        var entity = await _db.Set<T>().FindAsync([id], ct);
         if (entity == null || entity.IsDeleted) return null;
         return entity;
     }
 
     public IQueryable<T> GetQueryable() =>
-        appDbContext.Set<T>()
+        _db.Set<T>()
             .Where(e => !e.IsDeleted)
             .AsNoTracking()
             .AsQueryable();
@@ -46,7 +52,7 @@ public class BaseRepository<T>(AppDbContext appDbContext) : IBaseRepository<T> w
     public async Task UpdateAsync(T entity, CancellationToken ct = default)
     {
         entity.UpdatedAt = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
-        appDbContext.Set<T>().Update(entity);
-        await appDbContext.SaveChangesAsync(ct);
+        _db.Set<T>().Update(entity);
+        await _db.SaveChangesAsync(ct);
     }
 }
