fbfec9fa6fd327c93d62f185dc7fc73a503c5f40
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-17 18:57:47 +0300

    Added field for SearchUsersQuery

--- backend-pinterest/Application/UseCases/Users/Queries/SearchUsersQuery.cs
+++ backend-pinterest/Application/UseCases/Users/Queries/SearchUsersQuery.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Application.Models.DTO;
+using Application.Models.DTO.User;
+using MediatR;
 
 namespace Application.UseCases.Users.Queries;
 
-internal class SearchUsersQuery
+public record SearchUsersQuery : IRequest<PagedResult<UserDTO>>
 {
+    public string? Search { get; init; }
+    public int Page { get; init; } = 1;
+    public int PageSize { get; init; } = 20;
 }
