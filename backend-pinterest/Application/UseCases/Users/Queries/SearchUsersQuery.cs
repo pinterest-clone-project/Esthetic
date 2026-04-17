707f0fea95b1988411f3af4d55d04a980b6eea7d
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-17 19:10:08 +0300

    Enhance SearchUsersQuery with filters and sorting options

--- backend-pinterest/Application/UseCases/Users/Queries/SearchUsersQuery.cs
+++ backend-pinterest/Application/UseCases/Users/Queries/SearchUsersQuery.cs
@@ -1,4 +1,6 @@
-﻿using Application.Models.DTO;
+﻿using Application.Common.Sorting;
+using Application.Common.Sorting.User;
+using Application.Models.DTO;
 using Application.Models.DTO.User;
 using MediatR;
 
@@ -7,6 +9,10 @@ namespace Application.UseCases.Users.Queries;
 public record SearchUsersQuery : IRequest<PagedResult<UserDTO>>
 {
     public string? Search { get; init; }
+    public bool? IsPrivate { get; init; }
+    public bool? IsBlocked { get; init; }
+    public UserSortBy SortBy { get; init; } = UserSortBy.CreatedAt;
+    public SortDirection SortDirection { get; init; } = SortDirection.Asc;
     public int Page { get; init; } = 1;
     public int PageSize { get; init; } = 20;
 }
