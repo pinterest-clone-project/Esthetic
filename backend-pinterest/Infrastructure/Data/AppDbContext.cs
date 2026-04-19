ade5782cdd65fc322b1b8ebd6268e5c890ab426c
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-19 14:55:21 +0300

    Refactor DbContext to use assembly-based configurations

--- backend-pinterest/Infrastructure/Data/AppDbContext.cs
+++ backend-pinterest/Infrastructure/Data/AppDbContext.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
+using System.Reflection;
 
 namespace Infrastructure.Data;
 
@@ -34,58 +35,6 @@ public class AppDbContext : IdentityDbContext<UserEntity, RoleEntity, Guid,
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        builder.Entity<UserRoleEntity>(ur =>
-        {
-            ur.HasOne(ur => ur.Role)
-                .WithMany(r => r.UserRoles)
-                .HasForeignKey(r => r.RoleId)
-                .IsRequired();
-        });
-
-        builder.Entity<UserLoginEntity>(b =>
-        {
-            b.HasOne(l => l.User)
-                .WithMany(u => u.UserLogins)
-                .HasForeignKey(l => l.UserId)
-                .IsRequired();
-        });
-
-        builder.Entity<FollowEntity>(b =>
-        {
-            b.HasKey(f => new { f.FollowerId, f.FolloweeId });
-
-            b.HasOne(f => f.Follower)
-                .WithMany(u => u.Following)
-                .HasForeignKey(f => f.FollowerId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            b.HasOne(f => f.Followee)
-                .WithMany(u => u.Followers)
-                .HasForeignKey(f => f.FolloweeId)
-                .OnDelete(DeleteBehavior.Restrict);
-        });
-
-        builder.Entity<PinTagEntity>(entity =>
-        {
-            entity.HasKey(pt => new { pt.PinId, pt.TagId });
-
-            entity.HasOne(pt => pt.Pin)
-                .WithMany(p => p.PinTags)
-                .HasForeignKey(pt => pt.PinId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            entity.HasOne(pt => pt.Tag)
-                .WithMany(t => t.PinTags)
-                .HasForeignKey(pt => pt.TagId)
-                .OnDelete(DeleteBehavior.Cascade);
-        });
-
-        builder.Entity<PinEntity>(entity =>
-        {
-            entity.HasOne(p => p.Category)
-                .WithMany(c => c.Pins)
-                .HasForeignKey(p => p.CategoryId)
-                .OnDelete(DeleteBehavior.SetNull);
-        });
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
