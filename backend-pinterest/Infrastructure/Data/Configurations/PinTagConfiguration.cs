60796a42ae75f5d39fa4f50eb714b6a540560e41
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-19 14:53:33 +0300

    Implement PinTagEntity EF Core configuration

--- backend-pinterest/Infrastructure/Data/Configurations/PinTagConfiguration.cs
+++ backend-pinterest/Infrastructure/Data/Configurations/PinTagConfiguration.cs
@@ -1,9 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities.PinTag;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data.Configurations;
 
-internal class PinTagConfiguration
+public class PinTagConfiguration : IEntityTypeConfiguration<PinTagEntity>
 {
+    public void Configure(EntityTypeBuilder<PinTagEntity> builder)
+    {
+        builder.HasKey(pt => new { pt.PinId, pt.TagId });
+
+        builder.HasOne(pt => pt.Pin)
+            .WithMany(p => p.PinTags)
+            .HasForeignKey(pt => pt.PinId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(pt => pt.Tag)
+            .WithMany(t => t.PinTags)
+            .HasForeignKey(pt => pt.TagId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
