d68066b1524da3c9f78458a59cb8716fdb94b8b1
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-19 14:59:27 +0300

    Add Category-Pin relationship and update schema

--- backend-pinterest/Infrastructure/Migrations/20260419115703_Add_Category_Pin_Relationship.Designer.cs
+++ backend-pinterest/Infrastructure/Migrations/20260419115703_Add_Category_Pin_Relationship.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260419115703_Add_Category_Pin_Relationship")]
+    partial class Add_Category_Pin_Relationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
