88592d9616a3d7a3d51c6217548cb3d2b973d6cd
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-19 14:52:27 +0300

    Refactor FollowConfiguration for EF Core entity mapping

--- backend-pinterest/Infrastructure/Data/Configurations/FollowConfiguration.cs
+++ backend-pinterest/Infrastructure/Data/Configurations/FollowConfiguration.cs
@@ -1,9 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities.Follow;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data.Configurations;
 
-internal class FollowConfiguration
+public class FollowConfiguration : IEntityTypeConfiguration<FollowEntity>
 {
+    public void Configure(EntityTypeBuilder<FollowEntity> builder)
+    {
+        builder.HasKey(f => new { f.FollowerId, f.FolloweeId });
+
+        builder.HasOne(f => f.Follower)
+            .WithMany(u => u.Following)
+            .HasForeignKey(f => f.FollowerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(f => f.Followee)
+            .WithMany(u => u.Followers)
+            .HasForeignKey(f => f.FolloweeId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
