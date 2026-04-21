cb5c9ab80c51dd76ade1d6374961cf53c7d27f1e
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-21 18:44:28 +0300

    Refactor LikeHandler to use MediatR and DI

--- backend-pinterest/Application/UseCases/Pins/Handlers/LikeHandler.cs
+++ backend-pinterest/Application/UseCases/Pins/Handlers/LikeHandler.cs
@@ -1,9 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Application.UseCases.Pins.Commands;
+using Domain.Interfaces;
+using MediatR;
 
 namespace Application.UseCases.Pins.Handlers;
 
-internal class LikeHandler
+public class LikeHandler(ILikeRepository likeRepository) : IRequestHandler<LikeCommand, Unit>
 {
+    public async Task<Unit> Handle(LikeCommand request, CancellationToken cancellationToken)
+    {
+        await likeRepository.LikeAsync(request.UserId, request.PinId, cancellationToken);
+        return Unit.Value;
+    }
 }
