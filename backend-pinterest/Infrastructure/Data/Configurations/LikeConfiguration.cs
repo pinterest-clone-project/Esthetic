82ddd950a2fd0300384a63d09325c9f95a61fcad
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-21 18:18:02 +0300

    Refactor LikeConfiguration for EF Core join table

--- backend-pinterest/Infrastructure/Data/Configurations/LikeConfiguration.cs
+++ backend-pinterest/Infrastructure/Data/Configurations/LikeConfiguration.cs
@@ -1,9 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities.Like;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data.Configurations;
 
-internal class LikeConfiguration
+public class LikeConfiguration : IEntityTypeConfiguration<LikeEntity>
 {
+    public void Configure(EntityTypeBuilder<LikeEntity> builder)
+    {
+        builder.HasKey(l => new { l.PinId, l.UserId });
+
+        builder.HasOne(l => l.Pin)
+            .WithMany(p => p.Likes)
+            .HasForeignKey(l => l.PinId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(l => l.User)
+            .WithMany(u => u.Likes)
+            .HasForeignKey(l => l.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
