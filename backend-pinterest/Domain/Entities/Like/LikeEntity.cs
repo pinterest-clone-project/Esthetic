0dcd38ecfff82e32aaad15a91e7c55604d78f621
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-21 18:12:42 +0300

    Refactor LikeEntity for EF mapping and add properties

--- backend-pinterest/Domain/Entities/Like/LikeEntity.cs
+++ backend-pinterest/Domain/Entities/Like/LikeEntity.cs
@@ -1,9 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities.Identity;
+using Domain.Entities.Pins;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities.Like;
 
-internal class LikeEntity
+[Table("Likes")]
+public class LikeEntity
 {
+    public Guid PinId { get; set; }
+    public PinEntity Pin { get; set; } = null!;
+
+    public Guid UserId { get; set; }
+    public UserEntity User { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
