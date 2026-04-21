8a16e35ed905e540f498b68b98d1c3fe93e047e4
Khajiitspy <artyrhamuliak@gmail.com>
2026-04-21 19:55:26 -0400

    Tags and Pins Connection migration

---
+++ backend-pinterest/Infrastructure/Migrations/20260421235304_Pin_Tag_Connection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pin_Tag_Connection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tbl_pin_tags",
+                columns: table => new
+                {
+                    PinsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TagsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_pin_tags", x => new { x.PinsId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_tbl_pin_tags_tbl_pins_PinsId",
+                        column: x => x.PinsId,
+                        principalTable: "tbl_pins",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tbl_pin_tags_tbl_tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "tbl_tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_pin_tags_TagsId",
+                table: "tbl_pin_tags",
+                column: "TagsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tbl_pin_tags");
+        }
+    }
+}
