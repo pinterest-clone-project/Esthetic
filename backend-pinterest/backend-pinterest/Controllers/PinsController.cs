a2d4545a26a7f7e000ae121e86abf1f15aafc0b7
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-21 18:37:16 +0300

    Added PinsController

---
+++ backend-pinterest/backend-pinterest/Controllers/PinsController.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_pinterest.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PinsController(IMediator mediator) : ControllerBase
+{
+}
