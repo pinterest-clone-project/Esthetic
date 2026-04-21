3963342b1b34197529cb8fadce1c2527c735c175
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-21 18:47:16 +0300

    Refactor UnlikeHandler to use MediatR and DI

--- backend-pinterest/Application/UseCases/Pins/Handlers/UnlikeHandler.cs
+++ backend-pinterest/Application/UseCases/Pins/Handlers/UnlikeHandler.cs
@@ -1,9 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Application.UseCases.Pins.Commands;
+using Domain.Interfaces;
+using MediatR;
 
 namespace Application.UseCases.Pins.Handlers;
 
-internal class UnlikeHandler
+public class UnlikeHandler(ILikeRepository likeRepository) : IRequestHandler<UnlikeCommand, Unit>
 {
+    public async Task<Unit> Handle(UnlikeCommand request, CancellationToken cancellationToken)
+    {
+        await likeRepository.UnlikeAsync(request.UserId, request.PinId, cancellationToken);
+        return Unit.Value;
+    }
 }
