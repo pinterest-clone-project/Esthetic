7819e3741ea21a2507a7048bc67bac82902f7471
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-22 19:37:19 +0300

    Add authorized delete comment endpoint to controller

--- backend-pinterest/backend-pinterest/Controllers/CommentsController.cs
+++ backend-pinterest/backend-pinterest/Controllers/CommentsController.cs
@@ -1,7 +1,11 @@
-﻿using Application.UseCases.Comments.Commands;
+﻿using Application.Common.Exceptions;
+using Application.Common.Validators;
+using Application.UseCases.Comments.Commands;
+using Domain.Constants;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace backend_pinterest.Controllers;
 
@@ -16,5 +20,22 @@ public class CommentsController(IMediator mediator) : ControllerBase
     {
         var comment = await mediator.Send(command);
         return Ok(comment);
+    }
+
+    [Authorize]
+    [HttpDelete("delete/{commentId}")]
+    public async Task<IActionResult> DeleteComment([FromRoute] Guid commentId)
+    {
+        var userId = User.FindFirstValue(JwtClaims.Id)
+            ?? throw new UnauthorizedException(ValidationMessages.ErrorUnauthorized);
+
+        var command = new DeleteCommentCommand
+        {
+            UserId = Guid.Parse(userId),
+            CommentId = commentId
+        };
+
+        var result = await mediator.Send(command);
+        return Ok(result);
     }
 }
