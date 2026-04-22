e533cd3dd538a3846086c21fcb33f1e1e3db20a4
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-22 18:08:08 +0300

    Refactor CommentEntity for EF Core and add navigation props

--- backend-pinterest/Domain/Entities/Comment/CommentEntity.cs
+++ backend-pinterest/Domain/Entities/Comment/CommentEntity.cs
@@ -1,9 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities.Base;
+using Domain.Entities.Identity;
+using Domain.Entities.Pins;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities.Comment;
 
-internal class CommentEntity
+[Table("Comments")]
+public class CommentEntity : BaseEntity
 {
+    public Guid PinId { get; set; }
+    public PinEntity Pin { get; set; } = null!;
+
+    public Guid UserId { get; set; }
+    public UserEntity User { get; set; } = null!;
+
+    public string Text { get; set; } = null!;
 }
