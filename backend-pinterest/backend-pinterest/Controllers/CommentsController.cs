6f703b7828ed786629c1b7611843766a4b47487b
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-22 19:27:45 +0300

    Add create comment endpoint with MediatR support

--- backend-pinterest/backend-pinterest/Controllers/CommentsController.cs
+++ backend-pinterest/backend-pinterest/Controllers/CommentsController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.UseCases.Comments.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend_pinterest.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class CommentsController : ControllerBase
+public class CommentsController(IMediator mediator) : ControllerBase
 {
+    [Authorize]
+    [HttpPost("create")]
+    [Consumes("multipart/form-data")]
+    public async Task<IActionResult> CreateComment([FromForm] CreateCommentCommand command)
+    {
+        var comment = await mediator.Send(command);
+        return Ok(comment);
+    }
 }
