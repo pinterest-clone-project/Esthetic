1c8dbf91c6f6dbe9bb9b4cbe47506e00af9e08b6
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-22 20:32:48 +0300

    Move UnlikeHandler

--- backend-pinterest/Application/UseCases/Likes/Handlers/UnlikeHandler.cs
+++ backend-pinterest/Application/UseCases/Likes/Handlers/UnlikeHandler.cs
@@ -1,8 +1,8 @@
-﻿using Application.UseCases.Pins.Commands;
+﻿using Application.UseCases.Likes.Commands;
 using Domain.Interfaces;
 using MediatR;
 
-namespace Application.UseCases.Pins.Handlers;
+namespace Application.UseCases.Likes.Handlers;
 
 public class UnlikeHandler(ILikeRepository likeRepository) : IRequestHandler<UnlikeCommand, Unit>
 {
