1db079d214c95f1d5f938284008237463b38fbf1
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-22 19:38:46 +0300

    Refactor GetCommentsByPinQuery to MediatR record

--- backend-pinterest/Application/UseCases/Comments/Queries/GetCommentsByPinQuery.cs
+++ backend-pinterest/Application/UseCases/Comments/Queries/GetCommentsByPinQuery.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Application.Models.DTO.Comment;
+using MediatR;
 
 namespace Application.UseCases.Comments.Queries;
 
-internal class GetCommentsByPinQuery
+public record GetCommentsByPinQuery : IRequest<List<CommentDTO>>
 {
+    public Guid PinId { get; init; }
 }
