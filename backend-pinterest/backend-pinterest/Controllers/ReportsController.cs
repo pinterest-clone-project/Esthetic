2b09325e0dbf959edbc587c93e6cc3e24f33e67e
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-23 18:43:08 +0300

    Add endpoint GetMyReports

--- backend-pinterest/backend-pinterest/Controllers/ReportsController.cs
+++ backend-pinterest/backend-pinterest/Controllers/ReportsController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.Common.Exceptions;
+using Application.Common.Validators;
+using Application.UseCases.Reports.Queries;
+using Domain.Constants;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace backend_pinterest.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ReportsController : ControllerBase
+public class ReportsController(IMediator mediator) : ControllerBase
 {
+    [Authorize]
+    [HttpGet("my")]
+    public async Task<IActionResult> GetMyReports()
+    {
+        var userId = User.FindFirstValue(JwtClaims.Id)
+            ?? throw new UnauthorizedException(ValidationMessages.ErrorUnauthorized);
+
+        var query = new GetMyReportsQuery { ReporterId = Guid.Parse(userId) };
+        var result = await mediator.Send(query);
+        return Ok(result);
+    }
 }
