555e6b01d35bca53d07ef1188b3f94c54ff36ef1
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-23 18:12:28 +0300

    Add properties to ReportEntity

--- backend-pinterest/Domain/Entities/Report/ReportEntity.cs
+++ backend-pinterest/Domain/Entities/Report/ReportEntity.cs
@@ -1,9 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities.Base;
+using Domain.Entities.Identity;
+using Domain.Entities.Pin;
+using Domain.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities.Report;
 
-internal class ReportEntity
+[Table("Reports")]
+public class ReportEntity : BaseEntity
 {
+    public Guid ReporterId { get; set; }
+    public UserEntity Reporter { get; set; } = null!;
+
+    public Guid? ReportedUserId { get; set; }
+    public UserEntity? ReportedUser { get; set; }
+
+    public Guid? ReportedPinId { get; set; }
+    public PinEntity? ReportedPin { get; set; }
+
+    public string Reason { get; set; } = null!;
+    public ReportStatus Status { get; set; } = ReportStatus.Pending;
 }
