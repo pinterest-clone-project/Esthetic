bf013a8e1d9ea5e86de8897711d065ad5c2d32e1
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-23 18:38:17 +0300

    Add properties to ReportDTO

--- backend-pinterest/Application/Models/DTO/Report/ReportDTO.cs
+++ backend-pinterest/Application/Models/DTO/Report/ReportDTO.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Application.Models.DTO.Report;
 
-namespace Application.Models.DTO.Report;
-
-internal class ReportDTO
+public class ReportDTO
 {
+    public Guid Id { get; set; }
+    public Guid ReporterId { get; set; }
+    public Guid? ReportedUserId { get; set; }
+    public Guid? ReportedPinId { get; set; }
+    public string Reason { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
 }
