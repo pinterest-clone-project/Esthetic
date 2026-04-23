ad9651f0fe68b170d79e3833489dcab0a2bfba97
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-23 19:01:43 +0300

    Implement IRequestHandler in GetAllReportsHandler

--- backend-pinterest/Application/UseCases/Reports/Handlers/GetAllReportsHandler.cs
+++ backend-pinterest/Application/UseCases/Reports/Handlers/GetAllReportsHandler.cs
@@ -1,9 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Application.Models.DTO.Report;
+using Application.UseCases.Reports.Queries;
+using AutoMapper;
+using Domain.Interfaces;
+using MediatR;
 
 namespace Application.UseCases.Reports.Handlers;
 
-internal class GetAllReportsHandler
+public class GetAllReportsHandler(
+    IReportRepository reportRepository,
+    IMapper mapper) : IRequestHandler<GetAllReportsQuery, List<ReportDTO>>
 {
+    public async Task<List<ReportDTO>> Handle(GetAllReportsQuery request, CancellationToken cancellationToken)
+    {
+        var reports = await reportRepository.GetAllAsync(cancellationToken);
+        return mapper.Map<List<ReportDTO>>(reports);
+    }
 }
