1f651b8f1927c7ef0790e4e51ac49f3ae558eeac
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-23 18:39:53 +0300

    Add mapping to ReportMapper

--- backend-pinterest/Application/Mappers/ReportMapper.cs
+++ backend-pinterest/Application/Mappers/ReportMapper.cs
@@ -1,9 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Application.Models.DTO.Report;
+using AutoMapper;
+using Domain.Entities.Report;
 
 namespace Application.Mappers;
 
-internal class ReportMapper
+public class ReportMapper : Profile
 {
+    public ReportMapper()
+    {
+        CreateMap<ReportEntity, ReportDTO>()
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
+    }
 }
