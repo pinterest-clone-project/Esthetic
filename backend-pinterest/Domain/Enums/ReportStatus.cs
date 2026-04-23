6f860f03b1cecb098f63738ae04bcf270d0382bf
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-23 18:09:58 +0300

    Add statuses

--- backend-pinterest/Domain/Enums/ReportStatus.cs
+++ backend-pinterest/Domain/Enums/ReportStatus.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Domain.Enums;
 
-namespace Domain.Enums;
-
-internal class ReportStatus
+public enum ReportStatus
 {
+    Pending,
+    Reviewed,
+    Resolved,
+    Dismissed
 }
