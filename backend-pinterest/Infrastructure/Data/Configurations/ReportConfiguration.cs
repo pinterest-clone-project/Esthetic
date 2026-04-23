410ef1d496711c1629fb04e23d0b00eee84bd0ca
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-23 18:18:10 +0300

    Add Configure method to ReportConfiguration

--- backend-pinterest/Infrastructure/Data/Configurations/ReportConfiguration.cs
+++ backend-pinterest/Infrastructure/Data/Configurations/ReportConfiguration.cs
@@ -1,9 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities.Report;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data.Configurations;
 
-internal class ReportConfiguration
+public class ReportConfiguration : IEntityTypeConfiguration<ReportEntity>
 {
+    public void Configure(EntityTypeBuilder<ReportEntity> builder)
+    {
+        builder.HasOne(r => r.Reporter)
+               .WithMany(u => u.SentReports)
+               .HasForeignKey(r => r.ReporterId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(r => r.ReportedUser)
+               .WithMany(u => u.ReceivedReports)
+               .HasForeignKey(r => r.ReportedUserId)
+               .OnDelete(DeleteBehavior.SetNull);
+
+        builder.HasOne(r => r.ReportedPin)
+               .WithMany(p => p.Reports)
+               .HasForeignKey(r => r.ReportedPinId)
+               .OnDelete(DeleteBehavior.SetNull);
+
+        builder.Property(r => r.Status)
+               .HasConversion<string>();
+    }
 }
