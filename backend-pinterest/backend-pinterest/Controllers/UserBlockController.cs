47a7896e7552303996d74bb9eea408fc1ff32fbf
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-24 17:09:19 +0300

    Add endpoint Block

---
+++ backend-pinterest/backend-pinterest/Controllers/UserBlockController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Validators;
+using Application.UseCases.BlockUsers.Commands;
+using Domain.Constants;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace backend_pinterest.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserBlockController(IMediator mediator) : ControllerBase
+{
+    private Guid CurrentUserId => Guid.Parse(
+        User.FindFirstValue(JwtClaims.Id)
+        ?? throw new UnauthorizedException(ValidationMessages.ErrorUnauthorized));
+
+    [HttpPut("{blockedId}")]
+    public async Task<IActionResult> Block([FromRoute] Guid blockedId)
+    {
+        var result = await mediator.Send(new BlockUserCommand
+        {
+            BlockerId = CurrentUserId,
+            BlockedId = blockedId
+        });
+        return Ok(result);
+    }
+}
