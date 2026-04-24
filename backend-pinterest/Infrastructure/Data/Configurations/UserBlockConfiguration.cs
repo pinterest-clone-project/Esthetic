308b273347efa150b5f9d431f766951d9dac17c9
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-24 16:42:07 +0300

    Add method Configure

--- backend-pinterest/Infrastructure/Data/Configurations/UserBlockConfiguration.cs
+++ backend-pinterest/Infrastructure/Data/Configurations/UserBlockConfiguration.cs
@@ -1,9 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities.UserBlock;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data.Configurations;
 
-internal class UserBlockConfiguration
+public class UserBlockConfiguration : IEntityTypeConfiguration<UserBlockEntity>
 {
+    public void Configure(EntityTypeBuilder<UserBlockEntity> builder)
+    {
+        builder.HasKey(b => new { b.BlockerId, b.BlockedId });
+
+        builder.HasOne(b => b.Blocker)
+            .WithMany(u => u.BlockedUsers)
+            .HasForeignKey(b => b.BlockerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(b => b.Blocked)
+            .WithMany(u => u.BlockedByUsers)
+            .HasForeignKey(b => b.BlockedId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
