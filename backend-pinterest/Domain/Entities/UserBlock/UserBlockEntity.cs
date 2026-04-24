c379a51f7a5140a91fdf5317f66072cd9c3ef132
Taras <194535665+0Taras0@users.noreply.github.com>
2026-04-24 16:38:19 +0300

    Add properties to UserBlockEntity

--- backend-pinterest/Domain/Entities/UserBlock/UserBlockEntity.cs
+++ backend-pinterest/Domain/Entities/UserBlock/UserBlockEntity.cs
@@ -1,9 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities.UserBlock;
 
-internal class UserBlockEntity
+[Table("UserBlocks")]
+public class UserBlockEntity
 {
+    public Guid BlockerId { get; set; }
+    public UserEntity Blocker { get; set; } = null!;
+
+    public Guid BlockedId { get; set; }
+    public UserEntity Blocked { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
