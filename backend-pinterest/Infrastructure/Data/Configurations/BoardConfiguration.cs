8c8e20741aa5eeb9cfe49ad900ff642507c4cbca
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-04-30 14:08:16 +0300

    feat(boards): add Board and BoardPin entities with migration

---
+++ backend-pinterest/Infrastructure/Data/Configurations/BoardConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities.Board;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Infrastructure.Data.Configurations;
+
+public class BoardConfiguration : IEntityTypeConfiguration<BoardEntity>
+{
+     public void Configure(EntityTypeBuilder<BoardEntity> builder)
+     {
+              builder.HasOne(b => b.Owner)
+               .WithMany()
+               .HasForeignKey(b => b.OwnerId)
+               .OnDelete(DeleteBehavior.Cascade);
+      }
+}
+
