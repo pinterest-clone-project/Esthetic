35c88485b3437f771f4e7d1043984a584de781c1
Taras <194535665+0Taras0@users.noreply.github.com>
2026-05-02 13:00:21 +0300

    Implement BaseEntity in BoardSectionEntity

--- backend-pinterest/Domain/Entities/BoardSection/BoardSectionEntity.cs
+++ backend-pinterest/Domain/Entities/BoardSection/BoardSectionEntity.cs
@@ -1,9 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities.Base;
+using Domain.Entities.Board;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities.BoardSection;
 
-internal class BoardSectionEntity
+[Table("BoardSections")]
+public class BoardSectionEntity : BaseEntity
 {
+    public string Title { get; set; } = null!;
+
+    public Guid BoardId { get; set; }
+    public BoardEntity Board { get; set; } = null!;
+
+    public virtual ICollection<BoardPinEntity> BoardPins { get; set; } = new List<BoardPinEntity>();
 }
