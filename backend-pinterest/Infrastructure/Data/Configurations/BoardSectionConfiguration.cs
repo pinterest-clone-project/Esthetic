ee3efb0dfbef7e1876f30568a8b4b32a2d6323e6
Taras <194535665+0Taras0@users.noreply.github.com>
2026-05-02 13:11:36 +0300

    Add relations in BoardSectionConfiguration

--- backend-pinterest/Infrastructure/Data/Configurations/BoardSectionConfiguration.cs
+++ backend-pinterest/Infrastructure/Data/Configurations/BoardSectionConfiguration.cs
@@ -1,9 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities.BoardSection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data.Configurations;
 
-internal class BoardSectionConfiguration
+public class BoardSectionConfiguration : IEntityTypeConfiguration<BoardSectionEntity>
 {
+    public void Configure(EntityTypeBuilder<BoardSectionEntity> builder)
+    {
+        builder.HasOne(s => s.Board)
+            .WithMany(b => b.Sections)
+            .HasForeignKey(s => s.BoardId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(s => s.BoardPins)
+            .WithOne(bp => bp.Section)
+            .HasForeignKey(bp => bp.SectionId)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
 }
