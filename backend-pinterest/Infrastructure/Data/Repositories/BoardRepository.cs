48c3b706dec9a342fe8bf3295c83db2749645b21
Taras <194535665+0Taras0@users.noreply.github.com>
2026-05-02 13:33:49 +0300

    Add IBoardSectionRepository

--- backend-pinterest/Infrastructure/Data/Repositories/BoardRepository.cs
+++ backend-pinterest/Infrastructure/Data/Repositories/BoardRepository.cs
@@ -1,13 +1,10 @@
 ﻿using Domain.Entities.Board;
 using Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Infrastructure.Data.Repositories;
 
-    public class BoardRepository : BaseRepository<BoardEntity>, IBoardRepository
-    {
-        public BoardRepository(AppDbContext db) : base(db) { }
-    }
+public class BoardRepository : BaseRepository<BoardEntity>, IBoardRepository
+{
+    public BoardRepository(AppDbContext db) : base(db) { }
+}
 
