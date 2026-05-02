158be2c1ee98b2b06c2a75ee307e115098750c2e
Taras <194535665+0Taras0@users.noreply.github.com>
2026-05-02 13:04:15 +0300

    Add DbSet for BoardsSection

--- backend-pinterest/Infrastructure/Data/AppDbContext.cs
+++ backend-pinterest/Infrastructure/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 using Application.Interfaces;
 using Domain.Entities.Board;
+using Domain.Entities.BoardSection;
 using Domain.Entities.Category;
 using Domain.Entities.Comment;
 using Domain.Entities.Follow;
@@ -36,6 +37,7 @@ public class AppDbContext : IdentityDbContext<UserEntity, RoleEntity, Guid,
     public DbSet<UserBlockEntity> UserBlocks { get; set; }
     public DbSet<BoardEntity> Boards { get; set; }
     public DbSet<BoardPinEntity> BoardPins { get; set; }
+    public DbSet<BoardSectionEntity> BoardsSection { get; set; }
 
     public IDbContextTransaction? CurrentTransaction => Database.CurrentTransaction;
 
