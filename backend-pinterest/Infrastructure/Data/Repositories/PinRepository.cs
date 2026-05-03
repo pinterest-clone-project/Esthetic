dba53ff77294993026b35dca174a8a8d64589845
Taras <194535665+0Taras0@users.noreply.github.com>
2026-05-03 16:40:38 +0300

    Add PinRepository

---
+++ backend-pinterest/Infrastructure/Data/Repositories/PinRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities.Pin;
+using Domain.Interfaces;
+
+namespace Infrastructure.Data.Repositories;
+
+public class PinRepository : BaseRepository<PinEntity>, IPinRepository
+{
+    public PinRepository(AppDbContext db) : base(db) { }
+}
