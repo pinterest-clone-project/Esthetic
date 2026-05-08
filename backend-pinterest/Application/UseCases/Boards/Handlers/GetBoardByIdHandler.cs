7a9236e686647144cac99f5b54cd0ada7a24f0a9
Taras <194535665+0Taras0@users.noreply.github.com>
2026-05-08 14:35:57 +0300

    Add translation to GetBoardByIdHandler

--- backend-pinterest/Application/UseCases/Boards/Handlers/GetBoardByIdHandler.cs
+++ backend-pinterest/Application/UseCases/Boards/Handlers/GetBoardByIdHandler.cs
@@ -3,8 +3,6 @@ using Application.Common.Validators;
 using Application.Models.DTO.Board;
 using Application.UseCases.Boards.Queries;
 using AutoMapper;
-using AutoMapper.QueryableExtensions;
-using Domain.Entities.Board;
 using Domain.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -23,7 +21,7 @@ public class GetBoardByIdHandler(
             .Include(b => b.BoardPins)
                 .ThenInclude(bp => bp.Pin)
             .FirstOrDefaultAsync(b => b.Id == request.Id, cancellationToken)
-            ?? throw new NotFoundException("Board not found");
+            ?? throw new NotFoundException(ValidationMessages.NotFound(ValidationMessages.Board));
 
         return mapper.Map<BoardDetailsDTO>(board);
     }
