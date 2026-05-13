b21695fb51382baf0d52e309d787e01cd8823f05
Taras <194535665+0Taras0@users.noreply.github.com>
2026-05-13 19:06:53 +0300

    Refactor Optional as public generic struct with helpers

--- backend-pinterest/Application/Common/Optional/Optional.cs
+++ backend-pinterest/Application/Common/Optional/Optional.cs
@@ -1,9 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Application.Common.Optional;
 
-namespace Application.Common.Optional;
-
-internal class Optional
+public readonly struct Optional<T>
 {
+    public T? Value { get; }
+    public bool HasValue { get; }
+    public bool IsCleared { get; }
+
+    private Optional(T? value, bool hasValue, bool isCleared)
+    {
+        Value = value;
+        HasValue = hasValue;
+        IsCleared = isCleared;
+    }
+
+    public static Optional<T> From(T value) => new(value, hasValue: true, isCleared: false);
+    public static Optional<T> Clear() => new(default, hasValue: true, isCleared: true);
+    public static Optional<T> Undefined() => new(default, hasValue: false, isCleared: false);
+
+    public T? Apply(T? current) => IsCleared ? default : HasValue ? Value : current;
 }
