17fd840465d1375e7247c780275d1b559b562e26
Taras <194535665+0Taras0@users.noreply.github.com>
2026-05-13 19:09:03 +0300

    Make OptionalBinderProvider public and implement IModelBinderProvider

--- backend-pinterest/Application/Common/Optional/OptionalBinderProvider.cs
+++ backend-pinterest/Application/Common/Optional/OptionalBinderProvider.cs
@@ -1,9 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 
 namespace Application.Common.Optional;
 
-internal class OptionalBinderProvider
+public class OptionalBinderProvider : IModelBinderProvider
 {
+    public IModelBinder? GetBinder(ModelBinderProviderContext context)
+    {
+        if (context.Metadata.ModelType == typeof(Optional<string>))
+            return new BinderTypeModelBinder(typeof(OptionalBinder));
+
+        return null;
+    }
 }
