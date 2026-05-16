0e5955f637cda7c9b61e3e4b44c0dd356994c048
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-05-16 20:20:32 +0300

    Add google login endpoint

--- backend-pinterest/Application/Mappers/UserMapper.cs
+++ backend-pinterest/Application/Mappers/UserMapper.cs
@@ -1,4 +1,5 @@
-﻿using Application.Models.DTO.User;
+﻿using Application.Models.DTO;
+using Application.Models.DTO.User;
 using Application.UseCases.Account.Commands;
 using Application.UseCases.Users.Commands;
 using AutoMapper;
@@ -16,5 +17,11 @@ public class UserMapper : Profile
         CreateMap<UpdateUserCommand, UserEntity>()
             .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
         CreateMap<RegisterCommand, UserEntity>();
+
+        CreateMap<GoogleAccountModel, UserEntity>()
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+            .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Image, opt => opt.Ignore());
     }
 }
