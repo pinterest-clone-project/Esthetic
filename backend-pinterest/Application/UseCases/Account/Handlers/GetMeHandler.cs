a1944be9d33911da0a81770555288a2c82d49f79
Taras <194535665+0Taras0@users.noreply.github.com>
2026-05-29 12:46:32 +0300

    Refactor GetMeHandler to use DI and return UserDTO with roles
    Refactored GetMeHandler to be public and use dependency injection for UserManager and IMapper. Implemented IRequestHandler<GetMeQuery, UserDTO> to retrieve the current user, throw UnauthorizedException if not found, fetch user roles, and return a mapped UserDTO with roles. Added necessary using directives.

--- backend-pinterest/Application/UseCases/Account/Handlers/GetMeHandler.cs
+++ backend-pinterest/Application/UseCases/Account/Handlers/GetMeHandler.cs
@@ -1,9 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Application.Common.Exceptions;
+using Application.Common.Resources;
+using Application.Models.DTO.User;
+using Application.UseCases.Account.Queries;
+using AutoMapper;
+using Domain.Entities.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
 
 namespace Application.UseCases.Account.Handlers;
 
-internal class GetMeHandler
+public class GetMeHandler(
+    UserManager<UserEntity> userManager,
+    IMapper mapper)
+    : IRequestHandler<GetMeQuery, UserDTO>
 {
+    public async Task<UserDTO> Handle(GetMeQuery request, CancellationToken cancellationToken)
+    {
+        var user = await userManager.FindByIdAsync(request.UserId.ToString())
+            ?? throw new UnauthorizedException(ValidationMessages.ErrorUnauthorized);
+
+        var roles = await userManager.GetRolesAsync(user);
+
+        var dto = mapper.Map<UserDTO>(user);
+        dto.Roles = roles;
+
+        return dto;
+    }
 }
