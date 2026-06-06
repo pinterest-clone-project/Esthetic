0ca90f2eb4db056421f318790c6c83e813d741ca
P-Nastia <gogego1999@sectorid.com>
2026-06-06 14:28:45 +0300

    add title and blocks

--- frontend-pinterest/src/pages/home/FirstPage.tsx
+++ frontend-pinterest/src/pages/home/FirstPage.tsx
@@ -1,8 +1,28 @@
 const FirstPage = () =>{
     return (
-        <section className={"flex-col flex items-center pt-8"}>
-            <div className={"lg:text-[64px] md:text-5xl sm:text-4xl font-bold top-32 leading-[45px] tracking-normal text-center max-w-[1011px] text-white "}>Explore new ideas & inspirations</div>
+        <div>
+        <section className={"flex-col flex items-center pt-12"}>
+            <div className={"lg:text-[64px] text-4xl font-bold top-32 leading-[45px] tracking-normal text-center text-white "}>
+                Explore new ideas & inspirations</div>
+            <div className={"lg:text-[24px] text-xl top-32 leading-[45px] lg:pt-6 pt-3 tracking-normal text-center max-w-[1011px] text-[#A1A1A1A1] "}>
+                Discover and save your favourites from around the web</div>
+
+            {/* Categories */}
+            <div className="flex gap-3 mt-8">
+                {["Concept Art", "Photography", "Nature", "Wallpapers"].map(
+                    (category) => (
+                        <button
+                            key={category}
+                            className="bg-[#535353] px-8 py-2 rounded-[5px] w-[200px] h-[35px] text-white text-sm hover:bg-[#666] hover:cursor-pointer"
+                        >
+                            {category}
+                        </button>
+                    )
+                )}
+            </div>
         </section>
+
+        </div>
     )
 }
 export default FirstPage;
