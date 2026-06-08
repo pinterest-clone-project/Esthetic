1bc6ff99dd4c9b1b12ea2f76601ad4341d57bc9b
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-08 16:25:45 +0300

    Replace AutoMapper with custom UserMapper in RegisterHandler
    Refactored RegisterHandler to use a custom UserMapper instead of AutoMapper for mapping RegisterCommand to UserEntity. Updated constructor dependencies and mapping logic accordingly. Improved error handling and made minor formatting adjustments.

--- backend-pinterest/Application/UseCases/Account/Handlers/RegisterHandler.cs
+++ backend-pinterest/Application/UseCases/Account/Handlers/RegisterHandler.cs
@@ -1,11 +1,10 @@
 ﻿using Application.Common.Exceptions;
 using Application.Interfaces;
+using Application.Mappers;
 using Application.Models.DTO.User;
 using Application.UseCases.Account.Commands;
-using AutoMapper;
 using Domain.Constants;
 using Domain.Entities.Identity;
-using FluentValidation.Results;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 
@@ -14,34 +13,25 @@ namespace Application.UseCases.Account.Handlers;
 public class RegisterHandler(
     UserManager<UserEntity> userManager,
     RoleManager<RoleEntity> roleManager,
-    IMapper mapper,
+    UserMapper userMapper,
     IJwtTokenService tokenService,
-    IImageService imageService
-    ) : IRequestHandler<RegisterCommand, TokenDTO>
+    IImageService imageService) : IRequestHandler<RegisterCommand, TokenDTO>
 {
     public async Task<TokenDTO> Handle(RegisterCommand request, CancellationToken cancellationToken)
     {
-        var user = mapper.Map<UserEntity>(request);
+        var user = userMapper.ToEntity(request);
 
         if (request.ImageFile != null)
-        {
             user.Image = await imageService.SaveImageAsync(request.ImageFile);
-        }
 
         var createResult = await userManager.CreateAsync(user, request.Password);
         if (!createResult.Succeeded)
-        {
-            var errors = string.Join(", ", createResult.Errors.Select(e => e.Description));
-            throw new BadRequestException(errors);
-        }
+            throw new BadRequestException(string.Join(", ", createResult.Errors.Select(e => e.Description)));
 
         if (!await roleManager.RoleExistsAsync(Roles.User))
-        {
             await roleManager.CreateAsync(new RoleEntity { Name = Roles.User });
-        }
 
         await userManager.AddToRoleAsync(user, Roles.User);
-
         return await tokenService.CreateTokenAsync(user);
     }
 }
