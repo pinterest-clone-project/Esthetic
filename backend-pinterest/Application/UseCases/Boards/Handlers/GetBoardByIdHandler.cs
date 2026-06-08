b0c17b7efa86924f70de30148e17941fc7ccf92d
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-08 16:26:22 +0300

    Replace AutoMapper with custom BoardMapper in handler
    Removed IMapper dependency from GetBoardByIdHandler and replaced it with BoardMapper. Updated mapping logic to use BoardMapper.ToDetailsDto instead of AutoMapper for converting Board entities to BoardDetailsDTO.

--- backend-pinterest/Application/UseCases/Boards/Handlers/GetBoardByIdHandler.cs
+++ backend-pinterest/Application/UseCases/Boards/Handlers/GetBoardByIdHandler.cs
@@ -1,8 +1,8 @@
 ﻿using Application.Common.Exceptions;
 using Application.Common.Validators;
+using Application.Mappers;
 using Application.Models.DTO.Board;
 using Application.UseCases.Boards.Queries;
-using AutoMapper;
 using Domain.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -11,18 +11,15 @@ namespace Application.UseCases.Boards.Handlers;
 
 public class GetBoardByIdHandler(
     IBoardRepository boardRepository,
-    IMapper mapper) : IRequestHandler<GetBoardByIdQuery, BoardDetailsDTO>
+    BoardMapper boardMapper) : IRequestHandler<GetBoardByIdQuery, BoardDetailsDTO>
 {
-    public async Task<BoardDetailsDTO> Handle(
-        GetBoardByIdQuery request,
-        CancellationToken cancellationToken)
+    public async Task<BoardDetailsDTO> Handle(GetBoardByIdQuery request, CancellationToken cancellationToken)
     {
         var board = await boardRepository.GetQueryable()
-            .Include(b => b.BoardPins)
-                .ThenInclude(bp => bp.Pin)
+            .Include(b => b.BoardPins).ThenInclude(bp => bp.Pin)
             .FirstOrDefaultAsync(b => b.Id == request.Id, cancellationToken)
             ?? throw new NotFoundException(ValidationMessages.NotFound(ValidationMessages.Board));
 
-        return mapper.Map<BoardDetailsDTO>(board);
+        return boardMapper.ToDetailsDto(board);
     }
 }
