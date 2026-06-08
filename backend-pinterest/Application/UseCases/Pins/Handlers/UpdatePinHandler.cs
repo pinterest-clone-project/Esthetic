ac59341607b78ea302cdf2fcb0832bfb0177cdc3
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-08 16:33:19 +0300

    Replace AutoMapper with PinMapper in UpdatePinHandler
    Switched from AutoMapper to a custom PinMapper for updating pin entities. Updated constructor injection and mapping logic accordingly. Simplified null checks and cleaned up using directives.

--- backend-pinterest/Application/UseCases/Pins/Handlers/UpdatePinHandler.cs
+++ backend-pinterest/Application/UseCases/Pins/Handlers/UpdatePinHandler.cs
@@ -1,20 +1,22 @@
+using Application.Common.Validators;
+using Application.Mappers;
 using Application.UseCases.Pins.Commands;
-using AutoMapper;
 using Domain.Entities.PinTag;
 using Domain.Interfaces;
 using MediatR;
-using Application.Common.Validators;
 
 namespace Application.UseCases.Pins.Handlers;
-public class UpdatePinHandler(IPinRepository repository, IMapper mapper)
-    : IRequestHandler<UpdatePinCommand, Unit>
+
+public class UpdatePinHandler(
+    IPinRepository repository,
+    PinMapper pinMapper) : IRequestHandler<UpdatePinCommand, Unit>
 {
     public async Task<Unit> Handle(UpdatePinCommand request, CancellationToken cancellationToken)
     {
-        var pin = await repository.GetByIdWithDetailsAsync(request.Id, cancellationToken);
-        if (pin == null) throw new KeyNotFoundException(ValidationMessages.NotFound("Pin"));
+        var pin = await repository.GetByIdWithDetailsAsync(request.Id, cancellationToken)
+            ?? throw new KeyNotFoundException(ValidationMessages.NotFound("Pin"));
 
-        mapper.Map(request, pin);
+        pinMapper.Patch(request, pin);
 
         if (request.TagIds != null)
             pin.PinTags = request.TagIds
