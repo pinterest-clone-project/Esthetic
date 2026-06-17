234bcd16b91a948cc7866aa9df7ae1ad60788171
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-17 19:35:47 +0300

    Add GetChatMessagesQuery for retrieving chat messages
    Introduced GetChatMessagesQuery record implementing MediatR's IRequest to fetch chat messages by ChatId and UserId, returning a list of MessageDTO. Added necessary using directives.

---
+++ backend-pinterest/Application/UseCases/Chat/Queries/GetChatMessagesQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Models.DTO.Chat;
+using MediatR;
+
+namespace Application.UseCases.Chat.Queries;
+
+public record GetChatMessagesQuery(Guid ChatId, Guid UserId) : IRequest<List<MessageDTO>>;
