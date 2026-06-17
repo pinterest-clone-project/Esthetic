2eed5fee3767c09c20b3253dc6f6c58245d6463e
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-17 19:43:15 +0300

    Add SendMessageValidator with content validation rules
    Introduced SendMessageValidator using FluentValidation to ensure SendMessageCommand.Content is not empty and does not exceed 2000 characters, with error messages in Ukrainian.

---
+++ backend-pinterest/Application/UseCases/Chat/Validators/SendMessageValidator.cs
@@ -0,0 +1,14 @@
+﻿using Application.UseCases.Chat.Commands;
+using FluentValidation;
+
+namespace Application.UseCases.Chat.Validators;
+
+public class SendMessageValidator : AbstractValidator<SendMessageCommand>
+{
+    public SendMessageValidator()
+    {
+        RuleFor(x => x.Content)
+            .NotEmpty().WithMessage("Повідомлення не може бути порожнім")
+            .MaximumLength(2000).WithMessage("Повідомлення занадто довге");
+    }
+}
