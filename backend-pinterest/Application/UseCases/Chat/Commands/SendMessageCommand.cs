43c8bc73f7c465231eccd78935eb50dd78b7f8ce
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-17 19:41:39 +0300

    Add SendMessageCommand record for chat messaging
    Introduced SendMessageCommand in the new Application.UseCases.Chat.Commands namespace. This record encapsulates chat message data and implements IRequest<MessageDTO> for MediatR integration. Added necessary using directives.

---
+++ backend-pinterest/Application/UseCases/Chat/Commands/SendMessageCommand.cs
@@ -0,0 +1,6 @@
+﻿using Application.Models.DTO.Chat;
+using MediatR;
+
+namespace Application.UseCases.Chat.Commands;
+
+public record SendMessageCommand(Guid ChatId, Guid SenderId, string Content) : IRequest<MessageDTO>;
