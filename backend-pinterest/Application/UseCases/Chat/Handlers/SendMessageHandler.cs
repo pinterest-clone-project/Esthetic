90e2af72872a144d73a4e6aec915946362d7e414
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-17 19:45:14 +0300

    Add SendMessageHandler for chat message creation
    Implemented SendMessageHandler to process SendMessageCommand requests. The handler validates chat existence and user participation, handles exceptions, creates and saves new messages, and maps them to DTOs. Added required using directives.

---
+++ backend-pinterest/Application/UseCases/Chat/Handlers/SendMessageHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Exceptions;
+using Application.Mappers;
+using Application.Models.DTO.Chat;
+using Application.UseCases.Chat.Commands;
+using Domain.Entities.Chat;
+using Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.UseCases.Chat.Handlers;
+
+public class SendMessageHandler(
+    IMessageRepository messageRepository, IChatRepository chatRepository, MessageMapper messageMapper)
+    : IRequestHandler<SendMessageCommand, MessageDTO>
+{
+    public async Task<MessageDTO> Handle(SendMessageCommand request, CancellationToken ct)
+    {
+        var chat = await chatRepository.GetByIdAsync(request.ChatId, ct)
+            ?? throw new NotFoundException("Чат не знайдено");
+
+        if (chat.User1Id != request.SenderId && chat.User2Id != request.SenderId)
+            throw new ForbiddenException("Ви не є учасником цього чату");
+
+        var message = await messageRepository.AddAsync(new MessageEntity
+        {
+            ChatId = request.ChatId,
+            SenderId = request.SenderId,
+            Content = request.Content.Trim()
+        }, ct);
+
+        var dto = messageMapper.ToDTO(message);
+
+        return dto;
+    }
+}
