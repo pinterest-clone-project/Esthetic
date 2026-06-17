b160b8c8640b8e5401771c2e87a81fa163bc1a21
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-17 19:48:41 +0300

    Add MarkChatAsReadCommandHandler for marking chats as read
    Introduced MarkChatAsReadCommandHandler to handle marking chat messages as read for a user using IMessageRepository and MediatR. This enables asynchronous processing of read status updates in chat conversations.

---
+++ backend-pinterest/Application/UseCases/Chat/Handlers/MarkChatAsReadHandler.cs
@@ -0,0 +1,14 @@
+﻿using Application.UseCases.Chat.Commands;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.UseCases.Chat.Handlers;
+
+public class MarkChatAsReadCommandHandler(IMessageRepository messageRepository)
+    : IRequestHandler<MarkChatAsReadCommand>
+{
+    public async Task Handle(MarkChatAsReadCommand request, CancellationToken ct)
+    {
+        await messageRepository.MarkAsReadAsync(request.ChatId, request.UserId, ct);
+    }
+}
