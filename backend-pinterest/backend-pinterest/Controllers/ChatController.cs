a9653066b8ed574015d9dd77547792a36e0205f6
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-17 19:27:01 +0300

    Add ChatController with authorization and user ID extraction
    Introduced ChatController as an authorized API controller using IMediator for request handling. Added logic to extract the current user's ID from JWT claims and throw an UnauthorizedException if missing. Included necessary using directives for dependencies.

---
+++ backend-pinterest/backend-pinterest/Controllers/ChatController.cs
@@ -0,0 +1,19 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Validators;
+using Domain.Constants;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace backend_pinterest.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class ChatController(IMediator mediator) : ControllerBase
+{
+    private Guid CurrentUserId => Guid.Parse(
+        User.FindFirstValue(JwtClaims.Id)
+        ?? throw new UnauthorizedException(ValidationMessages.ErrorUnauthorized));
+}
