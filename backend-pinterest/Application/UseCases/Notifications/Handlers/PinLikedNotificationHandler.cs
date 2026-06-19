970b48d4c40f77930756b642fd5b7fb6a113efb8
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-19 14:38:36 +0300

    Add handler for pin liked notifications
    Implemented PinLikedNotificationHandler to create and send notifications when a user's pin is liked by another user, ensuring self-likes are ignored. Integrates with notification repository, notifier, and mapper.

---
+++ backend-pinterest/Application/UseCases/Notifications/Handlers/PinLikedNotificationHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common;
+using Application.Interfaces.Notifiers;
+using Application.Mappers;
+using Domain.Entities.Notification;
+using Domain.Enums;
+using Domain.Events;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.UseCases.Notifications.Handlers;
+
+public class PinLikedNotificationHandler(
+    INotificationRepository notificationRepository,
+    INotificationNotifier notifier,
+    NotificationMapper mapper)
+    : INotificationHandler<DomainEventNotification<PinLikedEvent>>
+{
+    public async Task Handle(DomainEventNotification<PinLikedEvent> e, CancellationToken ct)
+    {
+        if (e.Event.LikerId == e.Event.PinOwnerId) return;
+
+        var entity = await notificationRepository.AddAsync(new NotificationEntity
+        {
+            UserId = e.Event.PinOwnerId,
+            Type = NotificationType.Like,
+            Message = $"{e.Event.LikerUsername} вподобав ваш пін \"{e.Event.PinTitle}\"",
+            ActorId = e.Event.LikerId,
+            ActorUsername = e.Event.LikerUsername,
+            ActorImage = e.Event.LikerImage,
+            TargetId = e.Event.PinId
+        }, ct);
+
+        await notifier.NotifyAsync(e.Event.PinOwnerId, mapper.ToDTO(entity));
+    }
+}
