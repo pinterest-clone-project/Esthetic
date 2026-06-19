d0ac4ca9f35a793a5000ab99022157f5d6b110ba
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-19 14:58:02 +0300

    Add DI registrations for notification services and mappers
    Added INotificationRepository, SignalRNotificationNotifier, and NotificationMapper to the service container to support notification features. This enables dependency injection for notification repositories, notifiers, and entity mapping.

--- backend-pinterest/backend-pinterest/Extensions/WebApplicationBuilderExtensions.cs
+++ backend-pinterest/backend-pinterest/Extensions/WebApplicationBuilderExtensions.cs
@@ -230,6 +230,7 @@ public static class WebApplicationBuilderExtensions
         services.AddScoped<IPinRepository, PinRepository>();
         services.AddScoped<IChatRepository, ChatRepository>();
         services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<INotificationRepository, NotificationRepository>();
 
         services.AddScoped<IEmailJobScheduler, EmailJobScheduler>();
 
@@ -241,6 +242,7 @@ public static class WebApplicationBuilderExtensions
         services.AddScoped<ICookieService, CookieService>();
 
         services.AddSingleton<IChatNotifier, SignalRChatNotifier>();
+        services.AddSingleton<INotificationNotifier, SignalRNotificationNotifier>();
         #endregion
 
         #region Mappers
@@ -254,6 +256,7 @@ public static class WebApplicationBuilderExtensions
         services.AddSingleton<SeederMapper>();
         services.AddSingleton<ChatMapper>();
         services.AddSingleton<MessageMapper>();
+        services.AddSingleton<NotificationMapper>();
         #endregion
 
         #region OpenAPI
