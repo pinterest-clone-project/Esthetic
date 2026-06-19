61c53d102bb679603e14896d854652aa6f9bc3b8
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-19 14:34:27 +0300

    Add handler for user followed notifications
    Introduced UserFollowedNotificationHandler to process UserFollowedEvent domain events. The handler creates and saves a notification entity for the followee and sends a notification using the notifier and mapper services.

---
+++ backend-pinterest/Application/UseCases/Notifications/Handlers/UserFollowedNotificationHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common;
+using Application.Interfaces.Notifiers;
+using Application.Mappers;
+using Domain.Entities.Notification;
+using Domain.Enums;
+using Domain.Events;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.UseCases.Notifications.Handlers;
+
+public class UserFollowedNotificationHandler(
+    INotificationRepository notificationRepository,
+    INotificationNotifier notifier,
+    NotificationMapper mapper)
+    : INotificationHandler<DomainEventNotification<UserFollowedEvent>>
+{
+    public async Task Handle(DomainEventNotification<UserFollowedEvent> e, CancellationToken ct)
+    {
+        var entity = await notificationRepository.AddAsync(new NotificationEntity
+        {
+            UserId = e.Event.FolloweeId,
+            Type = NotificationType.Follow,
+            Message = $"{e.Event.FollowerUsername} почав стежити за вами",
+            ActorId = e.Event.FollowerId,
+            ActorUsername = e.Event.FollowerUsername,
+            ActorImage = e.Event.FollowerImage,
+            TargetId = null
+        }, ct);
+
+        await notifier.NotifyAsync(e.Event.FolloweeId, mapper.ToDTO(entity));
+    }
+}
