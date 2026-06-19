787065addc0a4df1837c33a6e0e24fb47cca107e
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-19 14:14:01 +0300

    Add update migration

---
+++ backend-pinterest/Infrastructure/Migrations/20260619111333_Update_Notifications.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Notifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ActorId",
+                table: "Notifications",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ActorImage",
+                table: "Notifications",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ActorUsername",
+                table: "Notifications",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TargetId",
+                table: "Notifications",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_ActorId",
+                table: "Notifications",
+                column: "ActorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_AspNetUsers_ActorId",
+                table: "Notifications",
+                column: "ActorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_AspNetUsers_ActorId",
+                table: "Notifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notifications_ActorId",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "ActorId",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "ActorImage",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "ActorUsername",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "TargetId",
+                table: "Notifications");
+        }
+    }
+}
