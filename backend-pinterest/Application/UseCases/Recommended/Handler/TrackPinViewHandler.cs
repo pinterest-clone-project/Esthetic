43522dd5467d85eac498ef17363e63d7ca1aa23b
P-Nastia <gogego1999@sectorid.com>
2026-06-27 13:27:52 +0300

    add logic to track what pins user has viewed

---
+++ backend-pinterest/Application/UseCases/Recommended/Handler/TrackPinViewHandler.cs
@@ -0,0 +1,36 @@
+﻿
+using Application.UseCases.Recommended.Command;
+using Domain.Entities.Recommended;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.UseCases.Recommended.Handler;
+
+public class TrackPinViewHandler(IRecommendedRepository repository) : IRequestHandler<TrackPinViewCommand, Unit>
+{
+    public async Task<Unit> Handle(TrackPinViewCommand command, CancellationToken cancellationToken)
+    {
+        var interaction = await repository.GetByUserPerPinAsync(command.UserId, command.PinId, cancellationToken);
+        if (interaction == null)
+        {
+            interaction = new UserPinInteraction
+            {
+                PinId = command.PinId,
+                UserId = command.UserId,
+                ViewCount = 1,
+                LastViewedAt = DateTime.UtcNow
+            };
+            await repository.AddAsync(interaction, cancellationToken);
+        }
+        else
+        {
+            interaction.ViewCount += 1;
+            interaction.LastViewedAt = DateTime.UtcNow;
+
+            await repository.UpdateAsync(interaction, cancellationToken);
+        }
+
+
+        return Unit.Value;
+    }
+}
---
+++ backend-pinterest/backend-pinterest/Controllers/RecommendedController.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Validators;
+using Application.UseCases.Recommended.Command;
+using Domain.Constants;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace backend_pinterest.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RecommendedController(IMediator mediator) : ControllerBase
+{
+    private Guid CurrentUserId => Guid.Parse(
+        User.FindFirstValue(JwtClaims.Id)
+        ?? throw new UnauthorizedException(ValidationMessages.ErrorUnauthorized));
+
+    [Authorize]
+    [HttpPost("track-view/{pinId}")]
+    public async Task<IActionResult> TrackView([FromRoute] Guid pinId)
+    {
+        var command = new TrackPinViewCommand
+        {
+            UserId = CurrentUserId,
+            PinId = pinId
+        };
+
+        var result = await mediator.Send(command);
+        return Ok(result);
+
+    }
+}
