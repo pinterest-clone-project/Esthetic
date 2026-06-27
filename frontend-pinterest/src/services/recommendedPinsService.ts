a9616c99a0ad3e9868aebcc7395a9bb870b0e369
P-Nastia <gogego1999@sectorid.com>
2026-06-27 17:08:29 +0300

    start get recommended logic

---
+++ frontend-pinterest/src/services/recommendedPinsService.ts
@@ -0,0 +1,22 @@
+import { api } from "./api.ts";
+import type { IPinSummaryResponse } from "../types/pin/responses/IPinSummaryResponse.ts";
+
+export const recommendedPinsService = api.injectEndpoints({
+    endpoints: (builder) => ({
+        getRecommendedPins: builder.query<IPinSummaryResponse[], void>({
+            query: () => ({ url: 'recommended/recommended', method: 'GET' }),
+            providesTags: ['RecommendedPins'],
+        }),
+        trackViewPin: builder.mutation<void, string>({
+            query: (pinId) => ({
+                url: `recommended/track-view/${pinId}`,
+                method: 'POST',
+            })
+        })
+    }),
+});
+
+export const {
+    useGetRecommendedPinsQuery,
+    useTrackViewPinMutation,
+} = recommendedPinsService;
