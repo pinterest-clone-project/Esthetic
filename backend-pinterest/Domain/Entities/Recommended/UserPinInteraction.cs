523e71e7e0a9440330c8d5b0c64deec49d900833
P-Nastia <gogego1999@sectorid.com>
2026-06-27 12:06:06 +0300

    created user pins interaction table for future recommended pins

---
+++ backend-pinterest/Domain/Entities/Recommended/UserPinInteraction.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.Identity;
+using Domain.Entities.Pin;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Recommended;
+
+[Table("users_pins_interactions")]
+public class UserPinInteraction
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid UserId { get; set; } 
+    public UserEntity User { get; set; } = null!;
+    public Guid PinId { get; set; }
+    public PinEntity Pin { get; set; } = null!;
+
+    public int ViewCount { get; set; } = 0;
+
+    public DateTime LastViewedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ backend-pinterest/Infrastructure/Migrations/20260627090436_Add_user_pins_interactions_table.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_user_pins_interactions_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastViewedAt",
+                table: "users_pins_interactions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "ViewCount",
+                table: "users_pins_interactions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastViewedAt",
+                table: "users_pins_interactions");
+
+            migrationBuilder.DropColumn(
+                name: "ViewCount",
+                table: "users_pins_interactions");
+        }
+    }
+}
