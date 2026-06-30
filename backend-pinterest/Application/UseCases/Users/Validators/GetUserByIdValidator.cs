8028b696dc44bb93e419622c054f2cf116174d19
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-30 15:42:42 +0300

    Add GetUserByIdValidator for user ID validation
    Introduced GetUserByIdValidator in the Users.Validators namespace. This validator enforces IdRules on the Id property of GetUserByIdQuery, ensuring consistent validation logic. Added necessary using directives.

---
+++ backend-pinterest/Application/UseCases/Users/Validators/GetUserByIdValidator.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Validators;
+using Application.UseCases.Users.Queries;
+using FluentValidation;
+
+namespace Application.UseCases.Users.Validators;
+
+public class GetUserByIdValidator : AbstractValidator<GetUserByIdQuery>
+{
+    public GetUserByIdValidator()
+    {
+        RuleFor(x => x.Id).IdRules();
+    }
+}
