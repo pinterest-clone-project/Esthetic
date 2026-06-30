8e75e9ffa29d2649db04ad72a1eb95daed6f8a4c
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-30 15:54:39 +0300

    Enhance user search and user model interfaces
    Added sortBy and sortDirection to ISearchUsersParams for improved user search capabilities. Extended IUser interface with isBlocked and blockReason fields to better represent user status.

--- frontend-pinterest/src/types/user/ISearchUsersParams.ts
+++ frontend-pinterest/src/types/user/ISearchUsersParams.ts
@@ -1,3 +1,6 @@
+import type {UserSortBy} from "@/types/user/UserSortBy.ts";
+import type {SortDirection} from "@/types/SortDirection.ts";
+
 export interface ISearchUsersParams {
     search?: string;
     isPrivate?: boolean;
