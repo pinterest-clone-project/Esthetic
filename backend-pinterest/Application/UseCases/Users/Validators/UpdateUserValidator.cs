4b9ce7d925e95a19c2e15b3d51b680d480a8b972
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-30 15:40:32 +0300

    Add UpdateUserValidator for UpdateUserCommand validation
    Introduced UpdateUserValidator using FluentValidation to enforce rules on UpdateUserCommand fields, including Id, Email, FirstName, and LastName, with format and length checks and custom validation messages.

---
+++ backend-pinterest/Application/UseCases/Users/Validators/UpdateUserValidator.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Validators;
+using Application.UseCases.Users.Commands;
+using Domain.Constants;
+using FluentValidation;
+
+namespace Application.UseCases.Users.Validators;
+
+public class UpdateUserValidator : AbstractValidator<UpdateUserCommand>
+{
+    public UpdateUserValidator()
+    {
+        RuleFor(x => x.Id).IdRules();
+
+        RuleFor(x => x.Email)
+            .EmailAddress()
+            .WithMessage(ValidationMessages.EmailFormat)
+            .MaximumLength(FieldLengths.EmailMax)
+            .WithMessage(ValidationMessages.MaxLength(ValidationMessages.FieldEmail, FieldLengths.EmailMax))
+            .When(x => !string.IsNullOrEmpty(x.Email));
+
+        RuleFor(x => x.FirstName)
+            .MaximumLength(FieldLengths.NameMax)
+            .WithMessage(ValidationMessages.MaxLength(ValidationMessages.FieldFirstName, FieldLengths.NameMax))
+            .When(x => x.FirstName is not null);
+
+        RuleFor(x => x.LastName)
+            .MaximumLength(FieldLengths.NameMax)
+            .WithMessage(ValidationMessages.MaxLength(ValidationMessages.FieldLastName, FieldLengths.NameMax))
+            .When(x => x.LastName is not null);
+    }
+}
