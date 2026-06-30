7e8aaf706cab93b75251cf61b1f0e8cc77ea8ef7
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-30 15:25:05 +0300

    Refactor GetUserByIdHandler to throw on missing user
    Changed handler to return non-nullable UserDTO and throw NotFoundException if user is not found, replacing the previous null return. Also consolidated using directives.

--- backend-pinterest/Application/UseCases/Users/Handlers/GetUserByIdHandler.cs
+++ backend-pinterest/Application/UseCases/Users/Handlers/GetUserByIdHandler.cs
@@ -1,3 +1,5 @@
+using Application.Common.Exceptions;
+using Application.Common.Validators;
 using Application.Mappers;
 using Application.Models.DTO.User;
 using Application.UseCases.Users.Queries;
@@ -8,12 +10,13 @@ namespace Application.UseCases.Users.Handlers;
 
 public class GetUserByIdHandler(
     IUserRepository userRepository,
-    UserMapper userMapper) : IRequestHandler<GetUserByIdQuery, UserDTO?>
+    UserMapper userMapper) : IRequestHandler<GetUserByIdQuery, UserDTO>
 {
-    public async Task<UserDTO?> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+    public async Task<UserDTO> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
     {
-        var user = await userRepository.GetByIdAsync(request.Id, cancellationToken);
-        if (user == null) return null;
+        var user = await userRepository.GetByIdAsync(request.Id, cancellationToken)
+            ?? throw new NotFoundException(ValidationMessages.UserNotFound);
+
         return userMapper.ToDto(user);
     }
 }
