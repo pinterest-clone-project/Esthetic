27fdbd94468fe37b128ff33002fbdddda07f392b
Taras <194535665+0Taras0@users.noreply.github.com>
2026-06-30 15:26:53 +0300

    UpdateUserCommand returns UserDTO and improves error handling
    UpdateUserCommand and its handler now return a UserDTO after updating a user, instead of Unit. Exception handling uses NotFoundException with a validation message. Repository methods now support CancellationToken. Added necessary using statements for DTOs, exceptions, and validators.

--- backend-pinterest/Application/UseCases/Users/Handlers/UpdateUserHandler.cs
+++ backend-pinterest/Application/UseCases/Users/Handlers/UpdateUserHandler.cs
@@ -1,4 +1,7 @@
+using Application.Common.Exceptions;
+using Application.Common.Validators;
 using Application.Mappers;
+using Application.Models.DTO.User;
 using Application.UseCases.Users.Commands;
 using Domain.Interfaces;
 using MediatR;
@@ -7,15 +10,15 @@ namespace Application.UseCases.Users.Handlers;
 
 public class UpdateUserHandler(
     IUserRepository userRepository,
-    UserMapper userMapper) : IRequestHandler<UpdateUserCommand, Unit>
+    UserMapper userMapper) : IRequestHandler<UpdateUserCommand, UserDTO>
 {
-    public async Task<Unit> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    public async Task<UserDTO> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
     {
-        var user = await userRepository.GetByIdAsync(request.Id)
-            ?? throw new KeyNotFoundException("User not found");
+        var user = await userRepository.GetByIdAsync(request.Id, cancellationToken)
+            ?? throw new NotFoundException(ValidationMessages.UserNotFound);
 
         userMapper.Patch(request, user);
-        await userRepository.UpdateAsync(user);
-        return Unit.Value;
+        await userRepository.UpdateAsync(user, cancellationToken);
+        return userMapper.ToDto(user);
     }
 }
