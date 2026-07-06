3bbcd01b1d3c8fbcfb65105ee8743cfb7ef410cf
Taras <194535665+0Taras0@users.noreply.github.com>
2026-07-06 14:59:11 +0300

    Add ReportQueryExtensions with ApplyStatusFilter method
    Introduced a static extension class for IQueryable<ReportEntity> to enable optional filtering by ReportStatus. Added necessary using directives for related entities and enums.

---
+++ backend-pinterest/Application/UseCases/Reports/Extensions/ReportQueryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities.Report;
+using Domain.Enums;
+
+namespace Application.UseCases.Reports.Extensions;
+
+public static class ReportQueryExtensions
+{
+    public static IQueryable<ReportEntity> ApplyStatusFilter(
+        this IQueryable<ReportEntity> query, ReportStatus? status)
+    {
+        return status.HasValue
+            ? query.Where(r => r.Status == status.Value)
+            : query;
+    }
+}
