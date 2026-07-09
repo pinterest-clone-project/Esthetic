bfc78378417e12bba5efd8122da3f5db9a50ff57
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-07-09 23:16:56 +0300

    update MoodboardPreviewPage

--- frontend-pinterest/src/pages/collections/CollectionsPage.tsx
+++ frontend-pinterest/src/pages/collections/CollectionsPage.tsx
@@ -135,7 +135,7 @@ const CollectionsPage = () => {
                                 <div className="w-full aspect-[4/3] rounded-2xl overflow-hidden bg-[#D1D1D1] dark:bg-[#2a2a2a] transition-transform duration-300 group-hover:scale-105 group-hover:shadow-2xl">
                                     {mb.coverImageUrl ? (
                                         <img
-                                            src={`${APP_ENV.IMAGES_800_URL}${mb.coverImageUrl}`}
+                                            src={`${APP_ENV.IMAGES_1200_URL}${mb.coverImageUrl}`}
                                             className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
                                         />
                                     ) : (
