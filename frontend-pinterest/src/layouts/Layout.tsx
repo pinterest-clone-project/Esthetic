4cc3a3c43b5639a70eb3d0298c42733f885be600
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-07-09 18:21:40 +0300

    revert: restore original Layout.tsx (min-h-screen, no overflow changes)

--- frontend-pinterest/src/layouts/Layout.tsx
+++ frontend-pinterest/src/layouts/Layout.tsx
@@ -9,12 +9,12 @@ const Layout = () => {
     const user = useAppSelector((state) => state.auth.user);
 
     return (
-        <div className="flex flex-col h-screen bg-white dark:bg-black">
+        <div className="flex flex-col min-h-screen bg-white dark:bg-black">
             <TopProgressBar />
             <Header />
-            <div className="flex flex-1 min-h-0 max-w-[1505px] mx-auto w-full">
+            <div className="flex flex-1 max-w-[1505px] mx-auto w-full">
                 {user && <Sidebar />}
-                <main className="flex-1 px-4 pb-20 md:pb-0 overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+                <main className="flex-1 px-4 scroll-smooth pb-20 md:pb-0">
                     <Outlet />
                 </main>
             </div>
