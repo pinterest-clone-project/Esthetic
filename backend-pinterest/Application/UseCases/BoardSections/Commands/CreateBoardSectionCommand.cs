9f1f490efc91070acbc558319284a63631e921a5
Nastia <nastyapivza1999@gmail.com>
2026-08-07 07:08:13 +0300

    add boardsectioncommand

---
+++ backend-pinterest/Application/UseCases/BoardSections/Commands/CreateBoardSectionCommand.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Application.Models.DTO.BoardSection;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Application.UseCases.BoardSections.Commands;
+
+public class CreateBoardSectionCommand : IRequest<BoardSectionDTO>
+{
+    public Guid BoardId { get; init; }
+    public required string Title { get; init; }
+
+    [BindNever]
+    public Guid OwnerId { get; init; }
+}
