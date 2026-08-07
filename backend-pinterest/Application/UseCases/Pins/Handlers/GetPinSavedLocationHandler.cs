fe5745fbc3b7c68254d71e436bde5d7e7db5ded1
Nastia <nastyapivza1999@gmail.com>
2026-08-07 12:50:05 +0300

    added method to distinguish where pin is: in board or inside it in section

---
+++ backend-pinterest/Application/UseCases/Pins/Handlers/GetPinSavedLocationHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Models.DTO.Pin;
+using Application.UseCases.Pins.Queries;
+using Domain.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Pins.Handlers;
+
+public class GetPinSavedLocationsHandler(
+    IBoardPinRepository boardPinRepository
+) : IRequestHandler<GetPinSavedLocationQuery, List<SavedPinLocationDTO>>
+{
+    public async Task<List<SavedPinLocationDTO>> Handle(
+        GetPinSavedLocationQuery request,
+        CancellationToken cancellationToken)
+    {
+        var locations = await boardPinRepository
+            .GetQueryable()
+            .Where(bp =>
+                bp.PinId == request.PinId &&
+                bp.Board.OwnerId == request.UserId &&
+                !bp.IsDeleted)
+            .Select(bp => new SavedPinLocationDTO
+            {
+                BoardId = bp.BoardId,
+                SectionId = bp.SectionId
+            })
+            .ToListAsync(cancellationToken);
+
+        return locations;
+    }
+}
