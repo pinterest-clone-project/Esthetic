05391fc620f63d5d755c00a6017b0c1f7e8b1883
Nastia <nastyapivza1999@gmail.com>
2026-08-07 08:49:47 +0300

    delete section handler

--- backend-pinterest/Application/Common/Validators/ValidationMessages.cs
+++ backend-pinterest/Application/Common/Validators/ValidationMessages.cs
@@ -66,6 +66,7 @@ public static class ValidationMessages
     public static string Board => Get("Board");
     public static string BoardSection => Get("BoardSection");
     public static string BoardDelOwnBoards => Get("BoardDelOwnBoards");
+    public static string BoardSectionDelOwnSections => Get("BoardSectionDelOwnSections");
     public static string BoardUpdateOwnBoards => Get("BoardUpdateOwnBoards");
     public static string Tag => Get("Tag");
     public static string EmailAlreadyExists => Get("EmailAlreadyExists");
---
+++ backend-pinterest/backend-pinterest/Controllers/BoardSectionsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_pinterest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardSectionsController : ControllerBase
+    {
+    }
+}
