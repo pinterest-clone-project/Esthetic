872ee28c604d438f594b0a48ea1d0749cb3f6cce
Nastia <nastyapivza1999@gmail.com>
2026-08-07 07:12:11 +0300

    delete command

---
+++ backend-pinterest/Application/UseCases/BoardSections/Commands/DeleteBoardSectionCommand.cs
@@ -0,0 +1,12 @@
+﻿
+using MediatR;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Application.UseCases.BoardSections.Commands;
+
+public class DeleteBoardSectionCommand : IRequest<Unit>
+{
+    public Guid Id { get; init; }
+    [BindNever]
+    public Guid OwnerId { get; init; }
+}
